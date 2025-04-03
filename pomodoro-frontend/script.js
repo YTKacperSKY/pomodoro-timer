a0095a844da2c0f1bab74d72aa4855107ab98a2c
Kacper Swonkowski <kacper.swonkowski@calimoto.com>
2025-04-03 14:19:47 +0200

    added auto reconnect

--- pomodoro-frontend/script.js
+++ pomodoro-frontend/script.js
@@ -19,6 +19,14 @@ let audioUnlocked = false;
 let connectedSessionId = null;
 let muteChime;
 
+setInterval(() => {
+    // check if ws is null or not open
+    if (!ws || ws.readyState !== WebSocket.OPEN) {
+        initWebSocket();
+        console.log('WebSocket timeout detected. Connection re-established...');
+    }
+}, 1000);
+
 function initWebSocket() {
   ws = new WebSocket('wss://pomodoro-be.ytkacpersky.de');
   ws.onopen = () => {
@@ -86,7 +94,6 @@ function connect() {
   if (!sessionId) {
     sessionId = getSessionIdFromUrlOrStorage();
     if (!sessionId) {
-      alert('Please enter a session name.');
       return;
     }
     document.getElementById('sessionId').value = sessionId;
